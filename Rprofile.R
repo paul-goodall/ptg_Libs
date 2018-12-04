42f5bc79b03799ad36a620de7a98e004d680b923
paul-goodall <paul.thomas.goodall@gmail.com>
2018-12-04 14:23:17 +1100

    Update Rprofile.R

--- Rprofile.R
+++ Rprofile.R
@@ -1,7 +1,7 @@
 cat("Good job on using the GitHub RProfile! :)\n")
 
 gitSource <- function(myRepo,myFile){
-  myURL <- "https://raw.githubusercontent.com/pgoodall1984/myRepo/master/myFile"
+  myURL <- "https://raw.githubusercontent.com/paul-goodall/myRepo/master/myFile"
   myURL <- gsub("myRepo",myRepo,myURL)
   myURL <- gsub("myFile",myFile,myURL)
   source(myURL)
